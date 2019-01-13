5e72c129e4d5309b2f1ac8a5cda97fbb58af4eb9
adidoank <42825332+adidoank@users.noreply.github.com>
2019-01-14 04:07:22 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_xxxxxxxxxxxxxxxxxxx";
+$raw_uid="xxxx-xxxx-xxxx-xxxx-xxxx";
+$user_name="User+name";
+$invite_code="xxxxxxx";
+$own_mac="02%xxxx%xxxx%xxxx%xxxx%xxxx";
+$android_id="xxxxxxxxxxxx";
+$device_type="xxxx++xxxx";
+
+
+?>
